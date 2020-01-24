2f2aa62d01c7035271502a1fcd8c3bf4b3ca27de
Randy Birdsey <birdseyrk@gmail.com>
2020-01-24 12:05:12 -0700

    Initial Commit

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Randy Birdsey 4/);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Randy Birdsey 4/);
+  expect(linkElement).toBeInTheDocument();
+});
