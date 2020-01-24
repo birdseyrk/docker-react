289b5b2c505798b63b06c4237d06e85a54c91f1b
Randy Birdsey <birdseyrk@gmail.com>
2020-01-24 13:38:45 -0700

    new test file

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Randy Birdsey 5/);
+  const linkElement = getByText(/Randy Birdsey 4/);
   expect(linkElement).toBeInTheDocument();
 });
